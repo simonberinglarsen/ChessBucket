2d1518364a4155376fb206c369a206202264abf7
Simon Bering Larsen <SLAR@danskebank.dk>
2016-08-30 15:00:54 +0200

    listen on all host headers

--- src/ChessBucket/Program.cs
+++ src/ChessBucket/Program.cs
@@ -14,6 +14,7 @@ namespace ChessBucket
             var host = new WebHostBuilder()
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseUrls("http://*:5000")
                 .UseIISIntegration()
                 .UseStartup<Startup>()
                 .Build();
